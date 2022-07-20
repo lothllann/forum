a9ea247f799606d212daab3ab9b577b4c36c88fa
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-07-20 12:09:44 -0300

    salvando

---
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -0,0 +1,28 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicoController {
+
+    @RequestMapping("/topicos")
+
+    public List<TopicoDto> Lista(){
+        Topico topico = new Topico("Duvida", "Duvida de Springboot", new Curso("Springboot","Programação"));
+        Topico topico2 = new Topico("Devtools", "Duvida de devtools", new Curso("Springboot","Programação"));
+        Topico topico3 = new Topico("Devtools dnovo", "Duvida de devtools denovo", new Curso("Springboot","Programação"));
+
+
+        return Arrays.asList(topico, topico2, topico3);
+    }
+
+}
---
+++ src/main/java/br/com/alura/forum/controller/TopicoDto.java
@@ -0,0 +1,12 @@
+package br.com.alura.forum.controller;
+
+import java.time.LocalDateTime;
+
+public class TopicoDto {
+    private Long id;
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataCriacao;
+}
+
+
