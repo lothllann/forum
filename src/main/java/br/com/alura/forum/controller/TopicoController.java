4d9c856733b58f129abed21dd8b81056f1a29495
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-07-22 16:13:50 -0300

    Topicos
    detalhando o JSON de retorno dos Topicos

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -1,20 +1,18 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.controller.dto.DetalhesDoTopicoDto;
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.controller.form.TopicoForm;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -36,6 +34,12 @@ public class TopicoController {
             List<Topico> topicos = tRepository.findByCurso_Nome(nomeCurso);
             return TopicoDto.converter(topicos);
         }
+    }
+
+    @GetMapping("/{id}")
+    public DetalhesDoTopicoDto detalhar(@PathVariable Long id){
+       Topico topico = tRepository.getOne(id);
+       return new DetalhesDoTopicoDto(topico);
     }
 
     @PostMapping
