47eddf3ce0a11c38f926c62f36f4f6fdc0b88092
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-07-27 16:55:03 -0300

    ordenação

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -34,9 +35,11 @@ public class TopicoController {
    @GetMapping
     public Page<TopicoDto> lista(@RequestParam (required = false) String nomeCurso,
                                  @RequestParam int pagina,
-                                 @RequestParam int qtd){
+                                 @RequestParam int qtd,
+                                 @RequestParam String ordem){
 
-       Pageable paginacao = PageRequest.of(pagina, qtd);
+       Pageable paginacao = PageRequest.of(pagina, qtd, Sort.Direction.ASC, ordem);
+
         if (nomeCurso == null){
         Page<Topico> topicos = tRepository.findAll(paginacao);
             return TopicoDto.converter(topicos);
