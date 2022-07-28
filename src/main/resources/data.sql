470269a74d8b0565dd83f4f3f924e2de0786867b
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-07-28 14:25:20 -0300

    Autorização
    implementando a logica de autorização do Spring

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$LvR4pVaLec4FMThpy8ApmOUP7G7V.ZTaj4HrPjAhG/U.X0Rl5BEx2');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
