ad2f2f3a5df2dca391d5f68bdcaeed4fab04d947
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-07-28 11:27:49 -0300

    Permissões Publicas
    Permissões Publicas do SpringBoot Security

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,5 +12,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    //CONFIGURAÇÃO DE AUTENTICAÇÃO
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {}
+
+    //CONFIGURAÇÃO DE AUTORIZAÇÃO
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers(HttpMethod.GET,"/topicos").permitAll()
+                .antMatchers(HttpMethod.GET,"/topicos/*").permitAll();
+    }
+
+    //CONFIGURAÇÃODE RECURSOS ESTATICOS(JS,CSS,HTML)
+    @Override
+    public void configure(WebSecurity web) throws Exception {}
 
 }
